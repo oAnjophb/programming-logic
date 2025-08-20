8d7372ef6a349dfc404df2a3382ad8460ce4fd22
Ruan Anjos <anjosruanp@gmail.com>
2025-08-20 18:11:59 -0300

    Add new problem solutions for BEECROWD: prob1197 and prob1198; create estudy.py for even number filtering

---
+++ BEECROWD/PYTHON/estudy.py
@@ -0,0 +1,3 @@
+x = [1,2,3,4,5,5,7,8]
+
+print(list(filter(lambda i: i % 2 == 0, x)))
---
+++ BEECROWD/PYTHON/prob1197.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        x, y = map(int, input().split())
+
+        print(x * y * 2)
+    
+    except EOFError:
+        break
